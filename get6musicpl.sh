ebe68a3bba37d4f5de30c7348180ea55f984349f
James Belchamber <james@belchamber.com>
2014-05-05 22:35:45 +0000

    created crappy bash script to scrape BBC 6 Music Playlists

--- get6musicpl.sh
+++ get6musicpl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl http://www.bbc.co.uk/6music/playlist|egrep "playlist-item-artist|playlist-item-title"|cut -d\> -f2|cut -d\< -f1|awk 'ORS=NR%2?" - ":"\n"'
+curl -s http://www.bbc.co.uk/6music/playlist|egrep "playlist-item-artist|playlist-item-title"|cut -d\> -f2|cut -d\< -f1|awk 'ORS=NR%2?" - ":"\n"'
