726f1a78771806bda66a08966a33a76cb947789d
Michael Crilly <mc@sent.com>
2016-09-14 22:25:53 +1000

    Update create_bbc_playlist.py
    Hopefully this will speed tihngs up a bit, but it's untested code as I don't have access to the relevant services.

--- create_bbc_playlist.py
+++ create_bbc_playlist.py
@@ -18,44 +18,50 @@ def decrypt_token(token):
 def verify_args():
   if not re.match(r"[^@]+@[^@]+\.[^@]+", username):
     print "Username invalid. Please enter your google username (probably your gmail address)"
-    return "fail"
-  passed = "fail"
-  for station in ["1xtra", "radio1", "radio2", "6music", "asiannetwork", "radioscotland"]:
-    if bbc_station == station:
-      passed = "pass"
-  if passed == "fail":
+    return False
+
+  passed = False
+
+  if not bbc_station in ["1xtra", "radio1", "radio2", "6music", "asiannetwork", "radioscotland"]:
     print "This is not a BBC Station (Or at least, not one which provides a playlist)"
-    return "fail"
-  return "pass"
+    return False
+
+  return True
 
 def google_login():
   logged_in = api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
-  if logged_in == 0:
+  if not logged_in:
     print "Login failed. Are you a Google Play Music All Access subscriber?"
+
   return logged_in
 
 def get_playlist():
   bbc_playlist = 'http://www.bbc.co.uk/' + bbc_station + '/playlist'
   r = requests.get(bbc_playlist)
   soup = BeautifulSoup(''.join(r))
+  
   songs = []
   for item in soup.findAll('div', { "class" : "pll-playlist-item-artist"}):
     if len(item.contents) == 3:
       songs.append(item.contents[1].string.strip())
     else:
       songs.append(item.string.strip())
+
   artists = []
   for item in soup.findAll('div', { "class" : "pll-playlist-item-title"}):
     artists.append(item.string)
+
   playlist_name = re.findall(r'<title>(.*?)</title>', r.text)
   artists = [string.encode('utf8', 'replace') for string in artists]
   songs = [string.encode('utf8', 'replace') for string in songs]
   playlist_name = playlist_name[0].encode('utf8', 'replace')
+ 
   count = 0
   titles = range(len(artists))
   while count != len(artists):
     titles[count] = artists[count] + " - " + songs[count]
-    count = count + 1
+    count += 1
+
   return titles, playlist_name
 
 def get_store_id(search_string):
@@ -74,12 +80,13 @@ def strip_non_ascii(string):
 def get_playlist_id(playlist):
   playlist = re.sub("- ", "", playlist)
   extant_playlists = api.get_all_playlists(incremental=False, include_deleted=False)
-  extant_playlist = "False"
-  for playlists in extant_playlists:
-    if playlists['name'] == playlist:
-      extant_playlist = "True"
-      playlist_id = playlists['id']
-  if extant_playlist == "True":
+  extant_playlist = False
+
+  if playlists['name'] in extant_playlists:
+    extant_playlist = True
+    playlist_id = playlists['id']
+
+  if extant_playlist:
     all_playlist_contents = api.get_all_user_playlist_contents()
     for playlist_contents in all_playlist_contents:
       if playlist_contents['id'] == playlist_id:
@@ -91,10 +98,11 @@ def get_playlist_id(playlist):
     return playlist_id
 
 def main():
-  if google_login() == 1:
+  if google_login():
     tracks_playlistname = get_playlist()
     tracks = tracks_playlistname[0]
     playlist = get_playlist_id(tracks_playlistname[1])
+
     for track in tracks:
       try:
         track = strip_non_ascii(track)
@@ -109,9 +117,11 @@ def handler(event, context):
   global username
   global password
   global bbc_station
+
   username = event['username']
   password = decrypt_token(event['password'])
   bbc_station = event['bbc_station']
+
   if verify_args() == "pass":
     main()
 
@@ -119,8 +129,10 @@ if __name__ == '__main__':
   global username
   global password
   global bbc_station
+  
   username = sys.argv[1]
   password = sys.argv[2]
   bbc_station = sys.argv[3]
-  if verify_args() == "pass":
+
+  if verify_args():
     main()
